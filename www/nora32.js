ff9feee1bbbe5fa0e0ebc2de5e2e9089aab3d1ff
Lionel Flandrin <lfg@svkt.eu>
2025-02-17 20:58:58 +0000

    Implement dithering

--- www/nora32.js
+++ www/nora32.js
@@ -5,7 +5,7 @@ redirectConsole("nora32-console");
 const wasm = await init();
 
 const canvas = document.getElementById('nora32-screen');
-const gl = canvas.getContext('webgl', { antialias: false });
+const gl = canvas.getContext('webgl2', { antialias: false });
 
 async function start() {
 
@@ -106,11 +106,11 @@ function compileShader(gl, type, source) {
     return shader;
 }
 
-const noRaVertexShader = `
-    attribute vec3 a_position;
-    attribute vec4 a_color;
+const noRaVertexShader = `#version 300 es
+    in vec3 a_position;
+    in vec4 a_color;
 
-    varying vec4 v_color;
+    out vec4 v_color;
 
     void main() {
         gl_Position = vec4(a_position, 1.0);
@@ -118,12 +118,36 @@ const noRaVertexShader = `
     }
 `;
 
-const noRaFragmentShader = `
+const noRaFragmentShader = `#version 300 es
     precision mediump float;
-    varying vec4 v_color;
+
+    in vec4 v_color;
+    out vec4 fragColor;
+
+    const float ditherMatrix[16] = float[16](
+    -0.5,   0.,   -0.375, 0.125,
+    0.25,  -0.25,  0.375, -0.125,
+    -0.375, 0.125, -0.5, 0.,
+    0.375, -0.125, 0.25, -0.25);
+
+    /* Artificially reduce color component resolution to 5 bits (to simulate
+     * RGB555) and add dithering */
+    vec4 dither(vec4 color, vec2 fragCoord) {
+        int xbias = int(mod(fragCoord.x, 4.));
+        int ybias = int(mod(fragCoord.y, 4.));
+
+        float bias = ditherMatrix[xbias + ybias *4];
+
+        return vec4(
+            round(color.r * 32. + bias) / 32.,
+            round(color.g * 32. + bias) / 32.,
+            round(color.b * 32. + bias) / 32.,
+            color.a
+            );
+    }
 
     void main() {
-        gl_FragColor = v_color;
+        fragColor = dither(v_color, gl_FragCoord.xy);
     }
 `;
 
